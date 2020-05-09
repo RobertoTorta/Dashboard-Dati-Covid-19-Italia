107a07a21a5fc8b741e7c5a4d2d877eedd283321
rober <rober@LAPTOP-SMI4J5CD.homenet.telecomitalia.it>
2020-05-09 13:30:03 +0200

    Primo commit

---
+++ Dashboard-Dati-Covid-19-Italia/src/main/java/polito/it/Dashboard_Dati_Covid_19_Italia/db/DatiCovidItaliaDAO.java
@@ -0,0 +1,16 @@
+package polito.it.Dashboard_Dati_Covid_19_Italia.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+
+public class DatiCovidItaliaDAO {
+	
+	public void popolaDB() {
+		
+	}
+
+}
---
+++ Dashboard-Dati-Covid-19-Italia/src/main/java/polito/it/Dashboard_Dati_Covid_19_Italia/model/Model.java
@@ -0,0 +1,5 @@
+package polito.it.Dashboard_Dati_Covid_19_Italia.model;
+
+public class Model {
+
+}
